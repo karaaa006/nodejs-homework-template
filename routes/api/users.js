d0b54d0a1c5dfe01cfcdc9b2941b05c9bfec1f9a
Oleh <olegkaraaa@gmail.com>
2022-02-02 23:41:47 +0200

    user schema, signup, login done

---
+++ routes/api/users.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const CreateError = require("http-errors");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const { User, schemas } = require("../../models/user");
+
+const router = express.Router();
+
+const { SECRET_KEY } = process.env;
+
+router.post("/signup", async (req, res, next) => {
+  try {
+    const { error } = schemas.signup.validate(req.body);
+
+    if (error) throw new CreateError(400, error.message);
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+    const user = await User.findOne({ email: req.body.email });
+    if (user) throw new CreateError(409, "Email in use");
+    const { email, subscription } = await User.create({
+      ...req.body,
+      password: hashedPass,
+    });
+
+    res.status(201).json({ email, subscription });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const { error } = schemas.login.validate(req.body);
+
+    if (error) throw new CreateError(400, error.message);
+
+    const user = await User.findOne({ email });
+
+    if (!user) throw new CreateError(401, "Email or password is wrong");
+
+    const compareResult = await bcrypt.compare(password, user.password);
+
+    if (!compareResult)
+      throw new CreateError(401, "Email or password is wrong");
+
+    const token = jwt.sign(user.id, SECRET_KEY);
+
+    res.json({
+      user: { email: user.email, subscription: user.subscription },
+      token,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
